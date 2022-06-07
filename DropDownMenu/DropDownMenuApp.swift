66d94db15e24d4b5836c5991d8007ed3a9d7224c
ABDELAZiZ EL ARASSi <elarassi.abdelaziz@gmail.com>
2022-06-07 17:32:45 +0100

    Initial Commit

---
+++ DropDownMenu/DropDownMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownMenuApp.swift
+//  DropDownMenu
+//
+//  Created by ABDELAZiZ EL ARASSi on 7/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct DropDownMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
